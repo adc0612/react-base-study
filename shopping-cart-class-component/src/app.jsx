aa6376888214664f93fa196adf41b0a8ab5816a3
Dohu <>
2022-05-15 20:50:21 +0900

    item component 생성 및 state, action 생성

--- shopping-cart-class-component/src/app.jsx
+++ shopping-cart-class-component/src/app.jsx
@@ -1,9 +1,11 @@
 import '@/app.css';
+import Items from './components/items';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>header</header>
+      <Items></Items>
     </div>
   );
 }
---
+++ shopping-cart-class-component/src/components/item.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.item);
+  };
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.item);
+  };
+  handleDelete = () => {
+    this.props.onDelete(this.props.item);
+  };
+  render() {
+    const { name, count } = this.props.item;
+    return (
+      <li>
+        <span className='item-name'>{name}</span>
+        <span className='item-count'>{count}</span>
+        <button className='item-button item-increase' onClick={this.handleIncrement}>
+          <i className='fas fa-plus-square'></i>
+        </button>
+        <button className='item-button item-decrease' onClick={this.handleDecrement}>
+          <i className='fas fa-minus-square'></i>
+        </button>
+        <button className='item-button item-delete' onClick={this.handleDelete}>
+          <i className='fas fa-trash'></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Item;
